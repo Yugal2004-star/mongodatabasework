87781509e9f9665a179d0e258fd55f9d5e4616ae
Yugal2004-star <yugaltembhare65@gmail.com>
2025-07-03 23:51:02 +0530

    indexmain

---
+++ index.js
@@ -0,0 +1,119 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const Chat = require("./models/chat.js");
+const methodOverride=require("method-override")
+
+// Set up EJS views
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+// Connect to MongoDB
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
+}
+
+main()
+  .then(() => {
+    console.log("Connection to MongoDB successful");
+
+    // Create and save chat after DB connection
+    let chat1 = new Chat({
+      from: "Ramen",
+      to: "ram",
+      msg: "good luck for your exams",
+      created_at: new Date()
+    });
+
+    chat1.save()
+      .then((res) => {
+        console.log("Chat saved:", res);
+      })
+      .catch((err) => {
+        console.error("Error saving chat:", err);
+      });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
+  //index route
+  app.get("/chats",async (req,res)=>{
+    let chats= await Chat.find();
+    //console.log(chats);
+    res.render("index.ejs",{chats});
+  })
+
+//new route
+app.get("/chats/new",(req,res)=>{
+  res.render("new.ejs")
+})
+
+//create route
+app.post("/chats", (req, res) => {
+  let { from, to, msg } = req.body;
+  let newChat = new Chat({
+    from: from,
+    to: to,
+    msg: msg,
+    created_at: new Date()
+  });
+  newChat
+  .save()
+  .then((res)=>{
+    console.log("chat was saved");
+  })
+  .catch((err)=>{
+    console.log(err);
+  })
+  res.redirect("/chats");
+  
+});
+
+//edit route
+app.get("/chats/:id/edit",async (req,res)=>{
+  let {id}=req.params;
+  let chat=  await Chat.findById(id);
+  
+  res.render("edit.ejs",{chat})
+});
+
+//update route
+app.put("/chats/:id",async (req,res)=>{
+    let {id}=req.params;
+    let {msg:newMsg}=req.body;
+    console.log(newMsg)
+    let updatedChat=await Chat.findByIdAndUpdate(id,{msg:newMsg},
+      {runValidators:true,
+      new:true
+    });
+  console.log(updatedChat);
+    res.redirect("/chats");
+
+});
+//destory route
+app.delete("/chats/:id",async (req,res)=>{
+  let {id}=req.params;
+  let deltedChat=await Chat.findByIdAndDelete(id);
+    console.log(deltedChat);
+      res.redirect("/chats");
+
+
+
+})
+
+  
+// Routes
+app.get("/", (req, res) => {
+  res.send("Root is working");
+});
+
+// Start server
+app.listen(8080, () => {
+  console.log("Server is listening on port 8080...");
+});
+
+
+
